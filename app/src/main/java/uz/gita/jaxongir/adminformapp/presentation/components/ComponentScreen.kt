b91515d7a13c50362e564a9dec10bc6f269d28a0
_primel <126654568+pr1me1@users.noreply.github.com>
2023-11-25 00:00:51 +0500

    id

--- app/src/main/java/uz/gita/jaxongir/adminformapp/presentation/components/ComponentScreen.kt
+++ app/src/main/java/uz/gita/jaxongir/adminformapp/presentation/components/ComponentScreen.kt
@@ -2,8 +2,9 @@ package uz.gita.jaxongir.adminformapp.presentation.components
 
 import androidx.compose.runtime.Composable
 import cafe.adriel.voyager.androidx.AndroidScreen
+import uz.gita.jaxongir.adminformapp.data.model.UserData
 
-class ComponentScreen(private val id: String): AndroidScreen() {
+class ComponentScreen(private val userData: UserData): AndroidScreen() {
     @Composable
     override fun Content() {
         TODO("Not yet implemented")
