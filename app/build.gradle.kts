1aaa6ec6b160e73bd17bdb9dd5e8f2e4b096a2f7
_primel <126654568+pr1me1@users.noreply.github.com>
2023-11-24 20:49:06 +0500

    Firebase added

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -66,4 +66,7 @@ dependencies {
     androidTestImplementation("androidx.compose.ui:ui-test-junit4")
     debugImplementation("androidx.compose.ui:ui-tooling")
     debugImplementation("androidx.compose.ui:ui-test-manifest")
+
+//    firebase
+    implementation(platform("com.google.firebase:firebase-bom:32.6.0"))
 }
