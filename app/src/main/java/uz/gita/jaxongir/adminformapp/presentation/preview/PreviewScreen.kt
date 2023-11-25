b2da12d04c8f9f0381b0e971b97a4048cc9e04c5
Malle- <isfan290902@gmail.com>
2023-11-25 17:11:31 +0500

    preview screen

--- app/src/main/java/uz/gita/jaxongir/adminformapp/presentation/preview/PreviewScreen.kt
+++ app/src/main/java/uz/gita/jaxongir/adminformapp/presentation/preview/PreviewScreen.kt
@@ -5,6 +5,8 @@ import androidx.annotation.RequiresApi
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
