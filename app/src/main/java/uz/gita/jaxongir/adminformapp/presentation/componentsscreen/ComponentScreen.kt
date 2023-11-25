82f919d049cae94fa244c1f65f82a6f42abe0ecd
Malle- <isfan290902@gmail.com>
2023-11-25 15:52:09 +0500

    edit components

--- app/src/main/java/uz/gita/jaxongir/adminformapp/presentation/componentsscreen/ComponentScreen.kt
+++ app/src/main/java/uz/gita/jaxongir/adminformapp/presentation/componentsscreen/ComponentScreen.kt
@@ -1,5 +1,6 @@
 package uz.gita.jaxongir.adminformapp.presentation.componentsscreen
 
+import android.annotation.SuppressLint
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -7,10 +8,13 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.material.OutlinedTextField
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material.Text
+import androidx.compose.material3.OutlinedTextFieldDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.State
 import androidx.compose.runtime.collectAsState
@@ -22,10 +26,13 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import cafe.adriel.voyager.androidx.AndroidScreen
 import cafe.adriel.voyager.hilt.getViewModel
+import com.google.firebase.components.ComponentContainer
 import uz.gita.jaxongir.adminformapp.data.enums.ComponentEnum
 import uz.gita.jaxongir.adminformapp.data.model.ComponentData
 import uz.gita.jaxongir.adminformapp.ui.components.SampleSpinner
@@ -43,43 +50,49 @@ class ComponentScreen(private val userId: String) : AndroidScreen() {
         viewModel.eventDispatcher(Contracts.Intent.Load(userId))
 
         MainContent(
+            userId = userId,
             uiState = viewModel.uiState.collectAsState(),
             onEventDispatcher = viewModel::eventDispatcher
         )
     }
+}
 
-    @Composable
-    fun MainContent(
-        uiState: State<Contracts.UIState>,
-        onEventDispatcher: (Contracts.Intent) -> Unit,
+@Composable
+fun MainContent(
+    userId: String,
+    uiState: State<Contracts.UIState>,
+    onEventDispatcher: (Contracts.Intent) -> Unit,
+) {
+    var type by remember {
+        mutableStateOf(ComponentEnum.SampleText)
+    }
+
+    var id by remember {
+        mutableStateOf("")
+    }
+
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(Color.White),
+        horizontalAlignment = Alignment.CenterHorizontally,
     ) {
-        var type by remember {
-            mutableStateOf(ComponentEnum.SampleText)
-        }
-
-        var id by remember {
-            mutableStateOf("")
-        }
-
-        Column(
-            modifier = Modifier.fillMaxSize(),
-            horizontalAlignment = Alignment.CenterHorizontally,
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .background(Color(0xFFff7686))
+                .padding(horizontal = 16.dp, vertical = 12.dp),
+            verticalAlignment = Alignment.CenterVertically
         ) {
-            Row(
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .background(Color(0xFFff7686))
-                    .padding(horizontal = 16.dp, vertical = 12.dp),
-                verticalAlignment = Alignment.CenterVertically
-            ) {
-                androidx.compose.material3.Text(
-                    text = "Add Component",
-                    color = Color.White,
-                    fontSize = 28.sp,
-                    fontWeight = FontWeight.Bold,
-                )
-            }
+            androidx.compose.material3.Text(
+                text = "Componenta Qo'shish",
+                color = Color.White,
+                fontSize = 28.sp,
+                fontWeight = FontWeight.Bold,
+            )
+        }
 
+        Column(modifier = Modifier.padding(horizontal = 16.dp)) {
 
             Spacer(modifier = Modifier.size(12.dp))
 
@@ -128,7 +141,12 @@ class ComponentScreen(private val userId: String) : AndroidScreen() {
                 },
                 label = {
                     Text(text = "Ixtiyorga qarab id qoshing")
-                }
+                },
+                modifier = Modifier.fillMaxWidth(),
+                colors = OutlinedTextFieldDefaults.colors(
+                    focusedBorderColor = Color(0xFFFF3951),
+                    unfocusedBorderColor = Color(0xFFFF7686)
+                )
             )
 
             Box(
@@ -163,8 +181,16 @@ class ComponentScreen(private val userId: String) : AndroidScreen() {
                     }
                 }
             }
-
-
         }
+
+
     }
 }
+
+@SuppressLint("UnrememberedMutableState")
+@Composable
+@Preview
+fun ComponentScreenPreview() {
+    MainContent("1", mutableStateOf(Contracts.UIState())) {}
+}
+
