19701286eebb5e88d50cdf7b582ddc88cb8e02c9
Jakhongir Ismoilov <ismoilovj2018@gmail.com>
2023-11-26 11:54:56 +0500

    Last changes

---
+++ app/src/main/java/uz/gita/jaxongir/adminformapp/ui/helper/ConditionsContent.kt
@@ -0,0 +1,51 @@
+package uz.gita.jaxongir.adminformapp.ui.helper
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import uz.gita.jaxongir.adminformapp.ui.previewitems.InputChipExample
+
+@Composable
+fun ConditionsContent(
+    list: List<String>,
+    modifier: Modifier
+) {
+    var variants by remember {
+        mutableStateOf(listOf<String>())
+    }
+
+    val newVariants = arrayListOf<String>()
+    newVariants.addAll(variants)
+
+    var selected by remember {
+        mutableStateOf(false)
+    }
+
+    Column(modifier = modifier.fillMaxWidth()) {
+        Spacer(modifier = Modifier.size(10.dp))
+        LazyRow(verticalAlignment = Alignment.CenterVertically) {
+            variants.forEachIndexed { index, s ->
+                item {
+                    list.forEach {
+                        InputChipExample(text = it) {
+
+                        }
+                        Spacer(modifier = Modifier.size(5.dp))
+                    }
+                }
+            }
+
+        }
+        Spacer(modifier = Modifier.size(10.dp))
+    }
+}
---
+++ app/src/main/java/uz/gita/jaxongir/adminformapp/ui/previewitems/InputChip.kt
@@ -0,0 +1,52 @@
+package uz.gita.jaxongir.adminformapp.ui.previewitems
+
+import androidx.compose.foundation.layout.size
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Close
+import androidx.compose.material.icons.filled.Person
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.InputChip
+import androidx.compose.material3.InputChipDefaults
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun InputChipExample(
+    text: String,
+    onDismiss: () -> Unit,
+) {
+    var enabled: Boolean by remember {
+        mutableStateOf(true)
+    }
+    if (!enabled) return
+
+    InputChip(
+        onClick = {
+            onDismiss()
+            enabled = !enabled
+        },
+        label = { Text(text) },
+        selected = enabled,
+        avatar = {
+            Icon(
+                Icons.Filled.Person,
+                contentDescription = "Localized description",
+                Modifier.size(InputChipDefaults.AvatarSize)
+            )
+        },
+        trailingIcon = {
+            Icon(
+                Icons.Default.Close,
+                contentDescription = "Localized description",
+                Modifier.size(InputChipDefaults.AvatarSize)
+            )
+        },
+    )
+}
