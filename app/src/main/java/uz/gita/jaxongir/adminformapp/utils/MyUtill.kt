7f57bfb232ba46d33840813732a401d5aa598f67
lobarashurova <106048869+lobarashurova@users.noreply.github.com>
2023-11-29 17:36:07 +0500

    all image updates

--- app/src/main/java/uz/gita/jaxongir/adminformapp/utils/MyUtill.kt
+++ app/src/main/java/uz/gita/jaxongir/adminformapp/utils/MyUtill.kt
@@ -1,7 +1,12 @@
 package uz.gita.jaxongir.adminformapp.utils
 
 import android.util.Log
+import androidx.compose.ui.unit.dp
 
 fun myLog(message :String) {
 //    Log.d("TTT", message)
+}
+
+fun String.toDp(): androidx.compose.ui.unit.Dp {
+    return this.toFloatOrNull()?.dp ?: 0.dp
 }
